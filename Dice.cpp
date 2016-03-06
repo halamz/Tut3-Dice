1ab1e4c011dc3690b10cf357d7c5faddebec4bcc
halamz <214519970@stu.ukzn.ac.za>
2016-03-06 10:34:57 +0200

    randomly generates values between 1 and 6

---
+++ Dice.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include<ctime>
+#include<cstdlib>
+#define n 5
+
+using namespace std;
+
+class Dice
+{
+private:
+
+public:
+	Dice();
+	int roll();
+	float average(Dice, int numRolls);
+	float average(int *Arr, int numValues);
+	~Dice();
+};
+
+//constructor
+Dice::Dice()
+{
+
+}
+
+//Method returns the value of the roll
+
+int Dice::roll()
+{
+	return  rand() % 5 + 1;
+}
+
+//Two globally overloaded functions
+float Dice::average(Dice obj, int numRolls)
+{
+	int sum = 0;
+	for (int i = 0; i<numRolls; i++)
+	{
+		sum += roll();
+	}
+	return float(sum / numRolls);
+}
+
+float Dice::average(int *arr, int numValues)
+{
+	float sum = 0;
+	for (int i = 0; i<numValues; i++)
+	{
+		sum += arr[i];
+	}
+	return sum / (float)numValues;
+}
+//Destructor
+Dice::~Dice()
+{
+
+}
+// main
+int main()
+{
+	srand((unsigned)time(0));// makes the randomly generated values, random
+	Dice die;
+	int arr[n];
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = die.roll();
+	}
+	cout << "Average using array : " << die.average(arr, n) << endl;
+	cout << "Average using Dice object : " << die.average(die, n) << endl;
+}
